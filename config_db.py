9a8cadf0fe253fa41df36ba2093f97accf71b354
Anderson Lopes <89695003+andersontropi@users.noreply.github.com>
2024-05-22 01:19:02 -0300

    Update config_db.py

--- config_db.py
+++ config_db.py
@@ -10,8 +10,8 @@ SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
         SGBD = 'mysql+mysqlconnector',
         usuario = 'sqluser',
-        senha = 'Univesp.2024',
-        servidor = '192.168.100.26',
+        senha = 'Univesp.2024',   #senha de exemplo#
+        servidor = 'localhost',
         database = 'satelite_prd'
     )
 
